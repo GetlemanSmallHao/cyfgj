394f23ac76b7b49a34cc38c9be4dbf12271a50d7
GentlemanSmallHao <307233239@qq.com>
2018-07-05 14:20:20 +0800

    [New] 新增办事指南页面

--- js/draw.js
+++ js/draw.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	document.getElementsByClassName('item_content')[0].onclick = function(){
+	/*document.getElementsByClassName('item_content')[0].onclick = function(){
 		window.location.href = "./draw_process.html";
-	}
+	}*/
 }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-	document.getElementsByTagName('li')[6].onclick = function(){
+	/*document.getElementsByTagName('li')[6].onclick = function(){
 		window.location.href = './draw.html';
-	}
+	}*/
 }

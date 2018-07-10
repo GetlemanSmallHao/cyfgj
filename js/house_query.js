a31c270d99d9c2d5755e4932284fa1f2503ff2da
GentlemanSmallHao <307233239@qq.com>
2018-07-10 11:03:15 +0800

    [New] 完成合同房查询页面

---
+++ js/house_query.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+	document.getElementsByClassName('query')[0].onclick = function(){
+		window.location.href = "./query_result.html";
+	};
+}
---
+++ utils/api.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName('back')[0].onclick = function(){
+	history.go(-1);
+}

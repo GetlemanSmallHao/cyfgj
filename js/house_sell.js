61e717809a71565148b60660359033abea374431
GentlemanSmallHao <307233239@qq.com>
2018-07-11 17:47:24 +0800

    [New] 完成了房屋出租首页

---
+++ js/house_sell.js
@@ -0,0 +1,30 @@
+$(function() {
+	$("[limit]").limit();
+	$('.house-tt').hide();
+	$('.house-tt').eq(0).show();
+	$('.house-tab .house-tab1').click(function(){
+		$('.small-k').removeClass("small-k-active");
+		$(this).children().addClass("small-k-active");
+		$('.house-tt').hide();
+		$('.house-tt').eq($(this).index()).fadeIn();
+	})
+
+})
+window.onload=window.onresize=function(){
+    var html=document.documentElement;
+    var width=html.clientWidth;
+    html.style.fontSize=100*(width/375)+'px';
+};
+//字符串截取封装函数  注:调用方法:$('.aa').tooltip();在.aa自定义类名limit="10"
+jQuery.fn.limit = function() {
+	var self = $("[limit]");
+	self.each(function() {
+		var objString = $(this).text();
+		var objLength = $(this).text().length;
+		var num = $(this).attr("limit");
+		if(objLength > num) {
+			$(this).attr("title", objString);
+			objString = $(this).text(objString.substring(0, num) + "...");
+		}
+	})
+}
